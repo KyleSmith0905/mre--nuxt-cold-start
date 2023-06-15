9ba5996d7fb2602fe97ced6226a01adf52d1f2f2
Kyle Smith <kylesmith090502@gmail.com>
2023-06-15 18:09:33 -0400

    Testing if database can lead to extended load times.

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,5 +1,22 @@
-import { NuxtAuthHandler } from '#auth';
+import { NuxtAuthHandler } from '#auth'
+import { PrismaClient } from '@prisma/client'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+
+const sleep = async (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+let prisma: PrismaClient;
+const getPrismaClient = async () => {
+  if (!prisma) return;
+
+  await sleep(10000);
+
+  prisma = new PrismaClient();
+  return prisma;
+}
 
 export default NuxtAuthHandler({
   secret: useRuntimeConfig().authSecret,
+  adapter: PrismaAdapter(getPrismaClient())
 });
