f1f7c7256d4ffe61bd2f48825d015e6d13f4172d
Kyle Smith <kylesmith090502@gmail.com>
2023-06-15 17:28:50 -0400

    fix page

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,4 @@
     <h1>Lorem, Ipsum</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, eaque exercitationem. Voluptatibus, doloremque. At sunt, odit quam id dolores dolor maxime dolorum, debitis officiis, ipsum deserunt eligendi sit aut et.</p>
   </div>
-  <NuxtPage />
 </template>
