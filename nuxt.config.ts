6bf12db294e295c3ca476fe29e5620a84b0eca87
Kyle Smith <kylesmith090502@gmail.com>
2023-06-15 17:13:51 -0400

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    authSecret: process.env.AUTH_SECRET,
+  },
+  routeRules: {
+    '/': {
+      prerender: true,
+    },
+  },
+
+  modules: ['@sidebase/nuxt-auth']
+})
